ffadcbfb0ca0188297cc1e127ef9b8b14e859268
Bharti <bharatifule10gmail.com>
2023-02-28 18:23:17 +0530

    Added:EmployeePayRollService_IO

--- main/java/com/bridgelabz/sql/Day27_Practice_Problem/EmployeePayRollMain.java
+++ main/java/com/bridgelabz/sql/Day27_Practice_Problem/EmployeePayRollMain.java
@@ -1,11 +1,12 @@
 package com.bridgelabz.sql.Day27_Practice_Problem;
 
 public class EmployeePayRollMain {
-	public static void main(String[] args) {
-        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-        System.out.println("Welcome to the Employee_Payroll_Services");
-        employeePayrollService.readEmployeeDataFromConsole();
-        employeePayrollService.writeEmployeeDataInConsole();
-    }
+	  public static void main(String[] args) {
+	        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+	        System.out.println("Welcome to the Employee_Payroll_Services");
+	        employeePayrollService.readEmployeeDataFromConsole();
+	        employeePayrollService.writeEmployeeDataInConsole();
+	    }
+	  
 	
 }
