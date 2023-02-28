4e1a9f145299a4fcf8459933b9c023bfbb357fd4
Bharti <bharatifule10gmail.com>
2023-02-28 18:11:46 +0530

    Added:EmployeePayRollService_IO

--- main/java/com/bridgelabz/sql/Day27_Practice_Problem/EmployeeData.java
+++ main/java/com/bridgelabz/sql/Day27_Practice_Problem/EmployeeData.java
@@ -1,20 +1,22 @@
 package com.bridgelabz.sql.Day27_Practice_Problem;
 
 public class EmployeeData {
-	private int employeeId;
-    private String employeeName;
-    private int employeeSalary;
+	 private int employeeId;
+	    private String employeeName;
+	    private int employeeSalary;
 
-    public EmployeeData(int employeeId, String employeeName, int employeeSalary) {
-        super();
-        this.employeeId = employeeId;
-        this.employeeName = employeeName;
-        this.employeeSalary = employeeSalary;
-    }
+	    public EmployeeData(int employeeId, String employeeName, int employeeSalary) {
+	        super();
+	        this.employeeId = employeeId;
+	        this.employeeName = employeeName;
+	        this.employeeSalary = employeeSalary;
+	    }
 
-    @Override
-    public String toString() {
-        return "employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeSalary=" + employeeSalary
-                + "";
-    }
+	    @Override
+	    public String toString() {
+	        return "employeeId=" + employeeId + ", employeeName=" + employeeName + ", employeeSalary=" + employeeSalary
+	                + "";
+	    }
+	    
 }
+
